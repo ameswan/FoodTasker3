4f57d095db328647a57d4708794bab56a981fe31
afwan70 <amirafwan94@gmail.com>
2018-11-03 07:49:39 +0800

    second test

---
+++ app/src/main/java/com/example/amirafwan/foodtasker/OrderListFragment.java
@@ -0,0 +1,29 @@
+package com.example.amirafwan.foodtasker;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class OrderListFragment extends Fragment {
+
+
+    public OrderListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_order_list, container, false);
+    }
+
+}
